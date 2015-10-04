dc9f536cf14f93eef1f3f9d38a52f1b00035f6ca
matt <mattmckillip@gmail.com>
2015-10-04 12:39:37 -0500

    just need to do the comparison, and i/o

---
+++ testingFiles/basicTest1.java
@@ -0,0 +1,25 @@
+package testingFiles;
+
+public class basicTest1 {
+	public String string1;
+	public String string2;
+	public Integer integer1;
+	public String integer2;
+	
+
+	public static void main(String[] args) {
+		int val;
+		funct1();
+		val = funct2(8);
+		val = funct2(-3);
+	}
+
+	public static void funct1() {
+		System.out.println("Inside funct1");
+	}
+
+	public static int funct2(int param) {
+		System.out.println("Inside funct2 with param " + param);
+		return param * 2;
+	}
+}
