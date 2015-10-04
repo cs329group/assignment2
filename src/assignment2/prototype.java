8c3f25ceb2c1082c7b053751983a820297274fdf
matt <mattmckillip@gmail.com>
2015-10-04 12:38:30 -0500

    now am using the JavaDataType class to store the data gathered from both classes

--- src/assignment2/prototype.java
+++ src/assignment2/prototype.java
@@ -7,51 +7,81 @@ import japa.parser.ast.body.MethodDeclaration;
 import japa.parser.ast.visitor.VoidVisitorAdapter;
 
 import java.io.FileInputStream;
+import java.util.ArrayList;
 
- 
 public class prototype {
-	public javaDataType test1 = new javaDataType("test1");
+	public static javaDataType tempFile;
 	public static void main(String[] args) throws Exception {
         // creates an input stream for the file to be parsed
-        FileInputStream in = new FileInputStream(System.getProperty("user.dir")+"\\src\\assignment2\\test1.java"); //It's been 4 years and i still dont know how to do this properly :(
+		String file1name = System.getProperty("user.dir")+"\\testingFiles\\basicTest1.java";
+		String file2name = System.getProperty("user.dir")+"\\testingFiles\\basicTest2.java";
+
+        FileInputStream file1 = new FileInputStream(file1name);
+        FileInputStream file2 = new FileInputStream(file2name); 
+        
+        javaDataType file1Data = new javaDataType(file1name);
+        javaDataType file2Data = new javaDataType(file2name);
        
-        CompilationUnit cu;
+        CompilationUnit cu1;
+        CompilationUnit cu2;
         try {
             // parse the file
-            cu = JavaParser.parse(in);
+            cu1 = JavaParser.parse(file1);
+            cu2 = JavaParser.parse(file2);
         } finally {
-            in.close();
+        	file1.close();
+        	file2.close();
+
         }
+        
+        tempFile = file1Data;
         // visit and print the methods names
-        new variableVisitor().visit(cu, null);
-        new MethodVisitor().visit(cu, null);
+        new variableVisitor().visit(cu1, null);
+        new MethodVisitor().visit(cu1, null);
+        
+        tempFile = file2Data;
+        new variableVisitor().visit(cu2, null);
+        new MethodVisitor().visit(cu2, null);
+        
+        System.out.println("File 1 Fields: " + file1Data.getFields().toString());
+        System.out.println("File 2 Fields: " + file2Data.getFields().toString());
+        System.out.println("");
+        System.out.println("File 1 Methods: " + file1Data.getMethods().toString());
+        System.out.println("File 2 Methods: " + file2Data.getMethods().toString());
+        System.out.println("");
+        System.out.println("File 1 Body: " + file1Data.getMethods().toString());
+        System.out.println("File 2 Body: " + file2Data.getMethods().toString());
+        System.out.println("");
+        System.out.println("File 1 Parameters: " + file1Data.getParameters().toString());
+        System.out.println("File 2 Parameters: " + file2Data.getParameters().toString());
+
     }
 
-    /**
-     * Simple visitor implementation for visiting MethodDeclaration nodes. 
-     */
     private static class MethodVisitor extends VoidVisitorAdapter {
 
         @Override
         public void visit(MethodDeclaration n, Object arg) {
-            // here you can access the attributes of the method.
-            // this method will be called for all methods in this 
-            // CompilationUnit, including inner class methods
-            System.out.println("Method: " + n.getName());
-            System.out.println("Parameter: " + n.getParameters());
-            System.out.println("Body: " + n.getBody() + "\n\n\n");
+        	String method = n.getName();
+        	String body = n.getBody().toString();
+        	String params;
+        	try{
+        		params =n.getParameters().toString();
+        	}
+        	catch (NullPointerException e){
+        		params = null;
+        	}
+        	
+            tempFile.addMethods(method);
+            tempFile.addBody(method,  n.getBody().toString());
+            tempFile.addParameter(method, params);
         }
     }
     
-    
-    /**
-     * Simple visitor implementation for visiting MethodDeclaration nodes. 
-     */
     private static class variableVisitor extends VoidVisitorAdapter {
 
     	@Override
     	public void visit(FieldDeclaration n, Object arg) {
-    	    System.out.println("Field: " + n);
+    	    tempFile.addField(n.toString());
     	    super.visit(n, arg);
     	}
     }
