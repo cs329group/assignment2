3bb216f4ecec0e1b78a79f13bd8432e9d9a45c16
matt <mattmckillip@gmail.com>
2015-10-04 12:37:54 -0500

    Added the setters and getters

--- src/assignment2/javaDataType.java
+++ src/assignment2/javaDataType.java
@@ -1,15 +1,47 @@
 package assignment2;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class javaDataType {
+	public HashMap<String, String> body = new HashMap<String, String>();
+	public HashMap<String, String> parameters = new HashMap<String, String>();
 	public ArrayList<String> methods = new	ArrayList<String>();
+	public ArrayList<String> fields = new	ArrayList<String>();
 	public String filename = null;
 	public javaDataType(String filename){
 		this.filename = filename;
 	}
 	
+	public void addMethods(String method){
+		methods.add(method);
+	}
+	
 	public ArrayList<String >getMethods(){
 		return methods;
+	}
+	
+	public void addField(String field){
+		fields.add(field);
+	}
+	
+	public ArrayList<String> getFields(){
+		return fields;
+	}
+	
+	public void addParameter(String method, String parameter){
+		parameters.put(method, parameter);
+	}
+	
+	public HashMap<String, String> getParameters(){
+		return parameters;
+	}
+	
+	public void addBody(String method, String body){
+		this.body.put(method, body);
+	}
+	
+	public HashMap<String, String> getBody(){
+		return body;
 	}
 }
